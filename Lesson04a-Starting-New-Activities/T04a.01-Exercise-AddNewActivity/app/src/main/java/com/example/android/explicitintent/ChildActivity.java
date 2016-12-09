a2f2a3be3df1f676d7f34fa2afaff8b59134457e
lwu <liang_wu@intuit.com>
2016-12-09 11:40:28 -0800

    Completed T4.1&4.2

---
+++ Lesson04a-Starting-New-Activities/T04a.01-Exercise-AddNewActivity/app/src/main/java/com/example/android/explicitintent/ChildActivity.java
@@ -0,0 +1,19 @@
+package com.example.android.explicitintent;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+public class ChildActivity extends AppCompatActivity {
+
+    private TextView mDisplayText;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_child);
+
+        mDisplayText = (TextView) findViewById(R.id.tv_display);
+    }
+}
--- Lesson04a-Starting-New-Activities/T04a.02-Exercise-StartNewActivity/app/src/main/java/com/example/android/explicitintent/MainActivity.java
+++ Lesson04a-Starting-New-Activities/T04a.02-Exercise-StartNewActivity/app/src/main/java/com/example/android/explicitintent/MainActivity.java
@@ -15,7 +15,10 @@
  */
 package com.example.android.explicitintent;
 
+import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -62,12 +65,15 @@ public class MainActivity extends AppCompatActivity {
                 Context context = MainActivity.this;
 
                 // TODO (1) Store ChildActivity.class in a Class object called destinationActivity
+                Class destinationActivity = ChildActivity.class;
 
                 // TODO (2) Create an Intent to start ChildActivity
+                Intent startChildActivityIntent = new Intent(context, destinationActivity);
 
                 // TODO (3) Replace the Toast with code to start ChildActivity
-                String message = "Button clicked!\nTODO: Start a new Activity and pass some data.";
-                Toast.makeText(context, message, Toast.LENGTH_LONG).show();
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+                    startActivity(startChildActivityIntent);
+                }
 
             }
         });
