382adcac7d0e0b6cfe743d2baee43efec1caa255
leonnnwu <wuliang0306@gmail.com>
2017-01-04 20:28:25 -0800

    Completed 10.1

--- Lesson08-Quiz-Example/T08.03-Exercise-FinishQuizExample/app/src/main/java/com/udacity/example/quizexample/MainActivity.java
+++ Lesson08-Quiz-Example/T08.03-Exercise-FinishQuizExample/app/src/main/java/com/udacity/example/quizexample/MainActivity.java
@@ -23,6 +23,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.udacity.example.droidtermsprovider.DroidTermsExampleContract;
 
@@ -39,6 +40,8 @@ public class MainActivity extends AppCompatActivity {
     private int mCurrentState;
 
     private Button mButton;
+    private TextView mWordTextView, mDefinitionTextView;
+    private int mWordCol, mDefinitionCol;
 
     // This state is when the word definition is hidden and clicking the button will therefore
     // show the definition
@@ -55,7 +58,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // Get the views
-        // TODO (1) You'll probably want more than just the Button
+        // Done (1) You'll probably want more than just the Button
+        mWordTextView = (TextView) findViewById(R.id.text_view_word);
+        mDefinitionTextView = (TextView) findViewById(R.id.text_view_definition);
         mButton = (Button) findViewById(R.id.button_next);
 
         //Run the database operation to get the cursor off of the main thread
@@ -87,11 +92,24 @@ public class MainActivity extends AppCompatActivity {
         // Change button text
         mButton.setText(getString(R.string.show_definition));
 
-        // TODO (3) Go to the next word in the Cursor, show the next word and hide the definition
+        // Done (3) Go to the next word in the Cursor, show the next word and hide the definition
         // Note that you shouldn't try to do this if the cursor hasn't been set yet.
         // If you reach the end of the list of words, you should start at the beginning again.
         mCurrentState = STATE_HIDDEN;
 
+        if(mData != null) {
+            if(!mData.moveToNext()) {
+                mData.moveToFirst();
+            }
+
+            mDefinitionTextView.setVisibility(View.INVISIBLE);
+            mButton.setText(getString(R.string.show_definition));
+
+            mWordTextView.setText(mData.getString(mWordCol));
+            mDefinitionTextView.setText(mData.getString(mDefinitionCol));
+
+            mCurrentState = STATE_HIDDEN;
+        }
     }
 
     public void showDefinition() {
@@ -99,7 +117,10 @@ public class MainActivity extends AppCompatActivity {
         // Change button text
         mButton.setText(getString(R.string.next_word));
 
-        // TODO (4) Show the definition
+        // Done (4) Show the definition
+        if(mData != null) {
+            mDefinitionTextView.setVisibility(View.VISIBLE);
+        }
         mCurrentState = STATE_SHOWN;
 
     }
@@ -130,8 +151,12 @@ public class MainActivity extends AppCompatActivity {
             // Set the data for MainActivity
             mData = cursor;
 
-            // TODO (2) Initialize anything that you need the cursor for, such as setting up
+            // Done (2) Initialize anything that you need the cursor for, such as setting up
             // the screen with the first word and setting any other instance variables
+            mWordCol = cursor.getColumnIndex(DroidTermsExampleContract.COLUMN_WORD);
+            mDefinitionCol = cursor.getColumnIndex(DroidTermsExampleContract.COLUMN_DEFINITION);
+
+            nextWord();
         }
     }
 
--- Lesson10-Hydration-Reminder/T10.01-Exercise-IntentServices/app/src/main/java/com/example/android/background/sync/WaterReminderIntentService.java
+++ Lesson10-Hydration-Reminder/T10.01-Exercise-IntentServices/app/src/main/java/com/example/android/background/sync/WaterReminderIntentService.java
@@ -1,3 +1,4 @@
+package com.example.android.background.sync;
 /*
  * Copyright (C) 2016 The Android Open Source Project
  *
@@ -14,10 +15,26 @@
  * limitations under the License.
  */
 
-// TODO (9) Create WaterReminderIntentService and extend it from IntentService
+import android.app.IntentService;
+import android.content.Intent;
 
-//  TODO (10) Create a default constructor that calls super with the name of this class
+public class WaterReminderIntentService extends IntentService {
 
-//  TODO (11) Override onHandleIntent
-//      TODO (12) Get the action from the Intent that started this Service
-//      TODO (13) Call ReminderTasks.executeTaskForTag and pass in the action to be performed
+    public WaterReminderIntentService() {
+        super(WaterReminderIntentService.class.getSimpleName());
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        String action = intent.getAction();
+        ReminderTasks.executeTask(this, action);
+    }
+}
+
+// Done (9) Create WaterReminderIntentService and extend it from IntentService
+
+//  Done (10) Create a default constructor that calls super with the name of this class
+
+//  Done (11) Override onHandleIntent
+//      Done (12) Get the action from the Intent that started this Service
+//      Done (13) Call ReminderTasks.executeTaskForTag and pass in the action to be performed
